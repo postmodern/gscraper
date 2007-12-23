1579fd9e203bd28b84850b8b584e6981d1fd6c7a
Postmodern <postmodern.mod3@gmail.com>
2007-12-23 04:51:45 +0000

    * Added support for scraping of "Cached" and "Similar Pages" URLs of Search Results.

--- lib/gscraper/search/page.rb
+++ lib/gscraper/search/page.rb
@@ -119,10 +119,6 @@ module GScraper
       #
       #   page.ranks # => [...]
       #
-      #   page.ranks do |rank|
-      #     puts ranks
-      #   end
-      #
       def ranks
         map { |result| result.rank }
       end
@@ -132,10 +128,6 @@ module GScraper
       # Page.
       #
       #   page.titles # => [...]
-      #
-      #   page.titles do |title|
-      #     puts title
-      #   end
       #
       def titles
         map { |result| result.title }
@@ -147,10 +139,6 @@ module GScraper
       #
       #   page.urls # => [...]
       #
-      #   page.urls do |url|
-      #     puts url
-      #   end
-      #
       def urls
         map { |result| result.url }
       end
@@ -161,12 +149,49 @@ module GScraper
       #
       #   page.summaries # => [...]
       #
-      #   page.summaries do |summary|
-      #     puts summary
-      #   end
-      #
       def summaries
         map { |result| result.summary }
+      end
+
+      #
+      # Returns an Array containing the cached URLs of the results within
+      # the Page.
+      #
+      #   page.cached_urls # => [...]
+      #
+      def cached_urls
+        map { |result| result.cached_url }
+      end
+
+      #
+      # Returns an Array containing the cached pages of the results within
+      # the Page. If _opts_ are given, they will be used in accessing the
+      # cached page.
+      #
+      #   page.cached_pages # => [...]
+      #
+      def cached_pages(opts={})
+        map { |result| result.cached_page(opts) }
+      end
+
+      #
+      # Returns an Array containing the similar Query URLs of the results
+      # within the Page.
+      #
+      #   page.similar_urls # => [...]
+      #
+      def similar_urls
+        map { |result| result.similar_url }
+      end
+
+      #
+      # Returns an Array containing the similar Queries of the results
+      # within the Page.
+      #
+      #   page.similar_queries # => [...]
+      #
+      def similar_queries
+        map { |result| result.similar_query }
       end
 
       #
@@ -210,9 +235,54 @@ module GScraper
       end
 
       #
+      # Iterates over each result's cached URLs within the Page, passing
+      # each to the given _block_.
+      #
+      #   each_cached_url { |url| puts url }
+      #
+      def each_cached_url(&block)
+        cached_urls.each(&block)
+      end
+
+      #
+      # Iterates over each result's cached pages within the Page, passing
+      # each to the given _block_. If _opts_ are given, they will be used
+      # in accessing the cached pages.
+      #
+      #   each_cached_page { |page| puts page.readlines }
+      #
+      def each_cached_page(opts={},&block)
+        cached_pages(opts).each(&block)
+      end
+
+      #
+      # Iterates over each result's similar Query URLs within the Page,
+      # passing each to the given _block_.
+      #
+      #   each_similar_url { |url| puts url }
+      #
+      def each_similar_url(&block)
+        similar_urls.each(&block)
+      end
+
+      #
+      # Iterates over each result's similar Query within the Page, passing
+      # each to the given _block_.
+      #
+      #   each_similar_query do |q|
+      #     q.first_page do |page|
+      #       puts page.urls.join("\n")
+      #     end
+      #   end
+      #
+      def each_similar_query(&block)
+        similar_queries.each(&block)
+      end
+
+      #
       # Returns the ranks of the results that match the specified _block_.
       #
-      #   page.ranks_of { |result result.title =~ /awesome/ }
+      #   page.ranks_of { |result| result.title =~ /awesome/ }
       #
       def ranks_of(&block)
         results_with(&block).ranks
@@ -221,7 +291,7 @@ module GScraper
       #
       # Returns the titles of the results that match the specified _block_.
       #
-      #   page.titles_of { |result result.url.include?('www') }
+      #   page.titles_of { |result| result.url.include?('www') }
       #
       def titles_of(&block)
         results_with(&block).titles
@@ -230,7 +300,7 @@ module GScraper
       #
       # Returns the urls of the results that match the specified _block_.
       #
-      #   page.urls_of { |result result.summary =~ /awesome pants/ }
+      #   page.urls_of { |result| result.summary =~ /awesome pants/ }
       #
       def urls_of(&block)
         results_with(&block).urls
@@ -240,10 +310,51 @@ module GScraper
       # Returns the summaries of the results that match the specified
       # _block_.
       #
-      #   page.summaries_of { |result result.title =~ /what if/ }
+      #   page.summaries_of { |result| result.title =~ /what if/ }
       #
       def summaries_of(&block)
         results_with(&block).summaries
+      end
+
+      #
+      # Returns the cached URLs of the results that match the specified
+      # _block_.
+      #
+      #   page.cached_urls_of { |result| result.title =~ /howdy/ }
+      #
+      def cached_urls_of(&block)
+        results_with(&block).cached_urls
+      end
+
+      #
+      # Returns the cached pages of the results that match the specified
+      # _block_. If _opts_ are given, they will be used in accessing
+      # the cached pages.
+      #
+      #   page.cached_pages_of { |result| result.title =~ /dude/ }
+      #
+      def cached_pages_of(opts={},&block)
+        results_with(&block).cached_pages(opts)
+      end
+
+      #
+      # Returns the similar query URLs of the results that match the
+      # specified _block_.
+      #
+      #   page.similar_urls_of { |result| result.title =~ /what if/ }
+      #
+      def similar_urls_of(&block)
+        results_with(&block).similar_urls
+      end
+
+      #
+      # Returns the similar Queries of the results that match the
+      # specified _block_.
+      #
+      #   page.similar_queries_of { |result| result.title =~ /hackety/ }
+      #
+      def similar_queries_of(&block)
+        results_with(&block).similar_queries
       end
 
     end
--- lib/gscraper/search/result.rb
+++ lib/gscraper/search/result.rb
@@ -14,15 +14,54 @@ module GScraper
       # Summary from the result page
       attr_reader :summary
 
+      # URL of the cached result page
+      attr_reader :cached_url
+
+      # URL of the similar results Query
+      attr_reader :similar_url
+
       #
       # Creates a new Result object with the given _rank_, _title_
-      # _summary_, _url_ and _size_.
+      # _summary_, _url_, _size_, _cache_url_ and _similar_url_.
       #
-      def initialize(rank,title,url,summary)
+      def initialize(rank,title,url,summary,cached_url=nil,similar_url=nil)
         @rank = rank
         @title = title
         @url = url
         @summary = summary
+        @cached_url = cached_url
+        @similar_url = similar_url
+      end
+
+      #
+      # Opens the URL of the cached page for the Result. If _opts_ are
+      # given, they will be used in accessing the cached page URL.
+      #
+      #   result.cached_page # => File
+      #
+      def cached_page(opts={})
+        if @cached_url
+          return GScraper.open(@cached_url,opts)
+        end
+      end
+
+      #
+      # Create a new Query for results that are similar to the Result. If
+      # a _block_ is given, it will be passed the newly created Query
+      # object.
+      #
+      #   result.similar_query # => Query
+      #
+      #   result.similar_query do |q|
+      #     q.first_page.each_url do |url|
+      #       puts url
+      #     end
+      #   end
+      #
+      def similar_query(&block)
+        if @similar_url
+          return Query.from_url(@similar_url,&block)
+        end
       end
 
       #
