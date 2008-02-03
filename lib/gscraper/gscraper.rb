a646712626d866ae9c06d47d4df69f50c6b5cced
Postmodern <postmodern.mod3@gmail.com>
2008-02-03 06:53:26 +0000

    * Split code from gscraper.rb into web_agent.rb.

--- lib/gscraper/gscraper.rb
+++ lib/gscraper/gscraper.rb
@@ -1,7 +1,16 @@
 require 'mechanize'
-require 'open-uri'
 
 module GScraper
+  # Common proxy port.
+  COMMON_PROXY_PORT = 8080
+
+  #
+  # Returns the +Hash+ of proxy information.
+  #
+  def GScraper.proxy
+    @@proxy ||= {:host => nil, :port => COMMON_PROXY_PORT, :user => nil, :pass => nil}
+  end
+
   #
   # Returns the supported GScraper User-Agent Aliases.
   #
@@ -13,7 +22,7 @@ module GScraper
   # Returns the GScraper User-Agent
   #
   def GScraper.user_agent
-    @@user_agent ||= nil
+    @@user_agent ||= GScraper.user_agent_aliases['Windows IE 6']
   end
 
   #
@@ -21,50 +30,5 @@ module GScraper
   #
   def GScraper.user_agent=(agent)
     @@user_agent = agent
-  end
-
-  #
-  # Opens the _uri_ with the given _opts_. The contents of the _uri_ will be
-  # returned.
-  #
-  #   GScraper.open('http://www.hackety.org/')
-  #
-  #   GScraper.open('http://tenderlovemaking.com/',
-  #     :user_agent_alias => 'Linux Mozilla')
-  #   GScraper.open('http://www.wired.com/', :user_agent => 'the future')
-  #
-  def GScraper.open(uri,opts={})
-    headers = {}
-
-    if opts[:user_agent_alias]
-      headers['User-Agent'] = WWW::Mechanize::AGENT_ALIASES[opts[:user_agent_alias]]
-    elsif opts[:user_agent]
-      headers['User-Agent'] = opts[:user_agent]
-    elsif GScraper.user_agent
-      headers['User-Agent'] = GScraper.user_agent
-    end
-
-    return Kernel.open(uri,headers)
-  end
-
-  #
-  # Creates a new Mechanize agent with the given _opts_.
-  #
-  #   GScraper.web_agent
-  #   GScraper.web_agent(:user_agent_alias => 'Linux Mozilla')
-  #   GScraper.web_agent(:user_agent => 'wooden pants')
-  #
-  def GScraper.web_agent(opts={})
-    agent = WWW::Mechanize.new
-
-    if opts[:user_agent_alias]
-      agent.user_agent_alias = opts[:user_agent_alias]
-    elsif opts[:user_agent]
-      agent.user_agent = opts[:user_agent]
-    elsif GScraper.user_agent
-      agent.user_agent = GScraper.user_agent
-    end
-
-    return agent
   end
 end
