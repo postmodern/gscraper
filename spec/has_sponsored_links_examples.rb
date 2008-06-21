aec7e187c10ff08c89e80a27571eee64dea15880
Postmodern <postmodern.mod3@gmail.com>
2008-06-21 08:27:34 +0000

    * Added the "has Sponsored Links" shared examples.

---
+++ spec/has_sponsored_links_examples.rb
@@ -0,0 +1,58 @@
+require 'pathname'
+require Pathname(__FILE__).dirname.join('spec_helper').expand_path
+
+shared_examples_for "has Sponsored Links" do
+
+  it "should have ads" do
+    @links.length.should_not == 0
+  end
+
+  it "should have titles" do
+    @links.each_title do |title|
+      title.should_not be_nil
+    end
+  end
+
+  it "should have non-empty titles" do
+    @links.each_title do |title|
+      title.length.should_not == 0
+    end
+  end
+
+  it "should have URLs" do
+    @links.each_url do |url|
+      url.should_not be_nil
+    end
+  end
+
+  it "should have non-empty URLs" do
+    @links.each_url do |url|
+      url.length.should_not == 0
+    end
+  end
+
+  it "should have valid URLs" do
+    @links.each_url do |url|
+      url_should_be_valid(url)
+    end
+  end
+
+  it "should have direct URLs" do
+    @links.each_direct_url do |url|
+      url.should_not be_nil
+    end
+  end
+
+  it "should have non-empty direct URLs" do
+    @links.each_direct_url do |url|
+      url.length.should_not == 0
+    end
+  end
+
+  it "should have valid direct URLs" do
+    @links.each_direct_url do |url|
+      url_should_be_valid(url)
+    end
+  end
+
+end
