e949a5a14a57568a59c866fddcd5e266c93526e5
Postmodern <postmodern.mod3@gmail.com>
2008-06-21 07:02:38 +0000

    * Added the "Page has Search Results" shared examples.

---
+++ spec/search/page_has_results_examples.rb
@@ -0,0 +1,45 @@
+require 'pathname'
+require Pathname(__FILE__).dirname.join('..','spec_helper').expand_path
+
+shared_examples_for "Page has Search Results" do
+
+  it "should have incremental ranks" do
+    ranks = @page.ranks
+
+    (0..(ranks.length - 2)).each do |index|
+      ranks[index].should < ranks[index + 1]
+    end
+  end
+
+  it "should have non-empty titles" do
+    @page.each_title do |title|
+      title.should_not be_nil
+      title.length.should_not == 0
+    end
+  end
+
+  it "should have non-empty URLs" do
+    @page.each_url do |url|
+      url.should_not be_nil
+      url.length.should_not == 0
+    end
+  end
+
+  it "should have valid URLs" do
+    @page.each_url do |url|
+      uri = URI(url)
+      uri.scheme.should_not be_nil
+      uri.host.should_not be_nil
+      uri.path.should_not be_nil
+    end
+  end
+
+  it "should have atleast one cached URL" do
+    @page.cached_urls.should_not == 0
+  end
+
+  it "should have atleast one similar query URL" do
+    @page.similar_urls.should_not == 0
+  end
+
+end
