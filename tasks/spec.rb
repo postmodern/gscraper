e8cbf911c1698c9fada1d0020e0f019b9bbd5203
Postmodern <postmodern.mod3@gmail.com>
2008-06-21 07:05:13 +0000

    * Don't wipe out t.libs.

--- tasks/spec.rb
+++ tasks/spec.rb
@@ -2,6 +2,6 @@ require 'spec/rake/spectask'
 
 desc "Run all specifications"
 Spec::Rake::SpecTask.new(:spec) do |t|
-  t.libs = ['lib', 'spec']
+  t.libs += [File.expand_path('lib'), File.expand_path('spec')]
   t.spec_opts = ['--colour', '--format', 'specdoc']
 end
