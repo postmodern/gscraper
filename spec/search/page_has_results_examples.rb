737c9c99c467bc44ca759ed86eec11ca8ec526c4
Postmodern <postmodern.mod3@gmail.com>
2008-06-21 08:31:16 +0000

    * Use the URI spec helper.

--- spec/search/page_has_results_examples.rb
+++ spec/search/page_has_results_examples.rb
@@ -11,26 +11,33 @@ shared_examples_for "Page has Search Results" do
     end
   end
 
-  it "should have non-empty titles" do
+  it "should have titles" do
     @page.each_title do |title|
       title.should_not be_nil
+    end
+  end
+
+  it "should have non-empty titles" do
+    @page.each_title do |title|
       title.length.should_not == 0
+    end
+  end
+
+  it "should have URLs" do
+    @page.each_url do |url|
+      url.should_not be_nil
     end
   end
 
   it "should have non-empty URLs" do
     @page.each_url do |url|
-      url.should_not be_nil
       url.length.should_not == 0
     end
   end
 
   it "should have valid URLs" do
     @page.each_url do |url|
-      uri = URI(url)
-      uri.scheme.should_not be_nil
-      uri.host.should_not be_nil
-      uri.path.should_not be_nil
+      url_should_be_valid(url)
     end
   end
 
