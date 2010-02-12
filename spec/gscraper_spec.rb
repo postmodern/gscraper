973521702b1ed0f9615a5cb59f114d078fd89ff4
postmodern <postmodern.mod3@gmail.com>
2010-02-11 19:04:35 -0800

    Updated specs a bit.

--- spec/gscraper_spec.rb
+++ spec/gscraper_spec.rb
@@ -1,8 +1,13 @@
 require 'spec_helper'
 
 require 'gscraper/gscraper'
+require 'gscraper/version'
 
 describe "GScraper" do
+  it "should have a VERSION constant" do
+    GScraper.should be_const_defined('VERSION')
+  end
+
   describe "User-Agent support" do
     it "should have a default User-Agent string" do
       GScraper.user_agent.should_not be_nil
@@ -11,19 +16,19 @@ describe "GScraper" do
 
   describe "Proxy support" do
     it "should provide a :host key" do
-      GScraper.proxy.has_key?(:host).should == true
+      GScraper.proxy.should have_key(:host)
     end
 
     it "should provide a :port key" do
-      GScraper.proxy.has_key?(:port).should == true
+      GScraper.proxy.should have_key(:port)
     end
 
     it "should provide a :user key" do
-      GScraper.proxy.has_key?(:user).should == true
+      GScraper.proxy.should have_key(:user)
     end
 
     it "should provide a :password key" do
-      GScraper.proxy.has_key?(:password).should == true
+      GScraper.proxy.should have_key(:password)
     end
   end
 end
