8370880a7b0c03b3c12a5ba3dfc19a6230187d6a
Postmodern <postmodern.mod3@gmail.com>
2012-04-26 14:29:47 -0700

    Use the .// syntax.

--- lib/gscraper/search/web_query.rb
+++ lib/gscraper/search/web_query.rb
@@ -400,7 +400,7 @@ module GScraper
           results_length.times do |index|
             result   = results[index]
             rank     = rank_offset + (index + 1)
-            link     = result.at('//h3/a')
+            link     = result.at('.//h3/a')
             title    = link.inner_text
             link_url = URI(link.get_attribute('href')).query_params['q']
             url      = URI(link_url)
